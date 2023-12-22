91afc509a1e7858cb68c0886787684f8293f7ac1
fizzyfog <idrissisarah@gmail.com>
2023-12-22 10:21:54 +0100

    Add comments to shell program

--- shell.c
+++ shell.c
@@ -13,14 +13,14 @@ int main(int argc, char **argv)
 	size_t length;
 	int loops = 1;
 	(void)argc;
-
+	/* handle Ctrl+C interruption */
 	signal(SIGINT, signal_handle);
 
 	while (1)
 	{
 		buffer = NULL, args = NULL;
 		length = 0;
-
+		/* print prompt if shell is from a terminal */
 		if (isatty(STDIN_FILENO))
 			write(STDOUT_FILENO, "$ ", 2);
 
@@ -31,18 +31,18 @@ int main(int argc, char **argv)
 			free(buffer);
 			return (0);
 		}
-		args = get_tokens(buffer);
+		args = get_tokens(buffer); /*tokenize input*/
 		if (args != NULL && args[0] != NULL)
 		{
-			if (exec_builtin(args, buffer, loops))
+			if (exec_builtin(args, buffer, loops)) /*check if builtin command*/
 				continue;
 
-			if (exec_local_program(args) == -1)
-				print_error(argv, args, loops);
+			if (exec_local_program(args) == -1) /*check if local program*/
+				print_error(argv, args, loops); /*print error if fail*/
 		}
 		free_memory(1, buffer);
 		free_memory(2, args);
 		loops++;
 	}
-	return (0);
+	return (0); /* exit shell on success */
 }
